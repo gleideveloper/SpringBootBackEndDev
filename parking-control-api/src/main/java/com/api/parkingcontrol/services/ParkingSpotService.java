97a968f1f4d08b500f0582ac2208711235a5f429
Gleides Vinente <gleidevelop@gmail.com>
2022-12-14 01:34:37 -0400

    Created method post to save the parkingSpot

---
+++ parking-control-api/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,21 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService {
+    private final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+    @Transactional
+    public Object save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
